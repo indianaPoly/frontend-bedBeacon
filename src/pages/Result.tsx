3575d04ff12781f5ad53ef016ebb7fc405478f90
george <hyunrim03@naver.com>
2024-05-06 21:52:51 +0900

    fix: id 추가

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Layout from '../components/Layout';
 import List from '../components/List';
@@ -7,48 +7,63 @@ import distanceToFloor from '../util/distanceToFloor';
 const Result = () => {
   const dummy = [
     {
+      id: 1,
       name: '고려대학교 구로병원',
       emergencyGeneralWard: 22,
       generalWard: 364,
       distance: 1300,
       location: '서울특별시 구로구 구로동로',
       callNumber: '02-2626-1114',
+      hospitalCode: '1',
+      image: '',
     },
     {
+      id: 2,
       name: '고려대학교 구로병원',
       emergencyGeneralWard: 22,
       generalWard: 364,
       distance: 7600,
       location: '서울특별시 구로구 구로동로',
       callNumber: '02-2626-1114',
+      hospitalCode: '2',
+      image: '',
     },
     {
+      id: 3,
       name: '고려대학교 구로병원',
       emergencyGeneralWard: 22,
       generalWard: 364,
       distance: 1331,
       location: '서울특별시 구로구 구로동로',
       callNumber: '02-2626-1114',
+      hospitalCode: '3',
+      image: '',
     },
     {
+      id: 4,
       name: '고려대학교 구로병원',
       emergencyGeneralWard: 22,
       generalWard: 364,
       distance: 500,
       location: '서울특별시 구로구 구로동로',
       callNumber: '02-2626-1114',
+      hospitalCode: '4',
+      image: '',
     },
     {
+      id: 5,
       name: '고려대학교 구로병원',
       emergencyGeneralWard: 22,
       generalWard: 364,
       distance: 100,
       location: '서울특별시 구로구 구로동로',
       callNumber: '02-2626-1114',
+      hospitalCode: '5',
+      image: '',
     },
   ];
 
-  const location = '서울특별시 구로구';
+  const [location, setLocation] = useState<string | null>();
 
   const navigate = useNavigate();
 
@@ -57,15 +72,29 @@ const Result = () => {
       sessionStorage.getItem('longitude') === null &&
       sessionStorage.getItem('latitude') === null
     ) {
-      navigate('/');
+      navigate('/select');
     }
   }, [navigate]);
+
+  // 위도 경도 값에 따른 지역구 파악해야됨.
+  useEffect(() => {
+    if (
+      sessionStorage.getItem('district') !== null &&
+      sessionStorage.getItem('city') !== null
+    ) {
+      setLocation(
+        `${sessionStorage.getItem('city') as string} ${sessionStorage.getItem(
+          'district'
+        )}`
+      );
+    }
+  }, []);
 
   return (
     <Layout>
       <div className="desktop:w-[900px] gap-[15px] flex flex-col">
         <div className=" w-full flex justify-center items-center gap-[3px]">
-          <span className="text-[12px] font-bold text-orange-700">
+          <span className="text-[12px] font-bold text-[#4095BD]">
             {location}
           </span>
           <span className="text-[12px] font-normal text-gray-500">
@@ -75,13 +104,15 @@ const Result = () => {
         <div className="grid place-items-center mobile:flex mobile:flex-col deskTop:grid deskTop:grid-cols-2 justify-center items-center gap-[20px]">
           {dummy.map((item) => (
             <List
-              key={item.name}
+              key={item.id}
               name={item.name}
               emergencyGeneralWard={item.emergencyGeneralWard}
               generalWard={item.generalWard}
               distance={distanceToFloor(item.distance)}
               location={item.location}
               callNumber={item.callNumber}
+              hospitalCode={item.hospitalCode}
+              image={item.image}
             />
           ))}
         </div>
