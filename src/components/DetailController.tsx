cbb3799a958f856dccee29cb0920f627ad14c041
george <hyunrim03@naver.com>
2024-05-08 20:31:07 +0900

    feat: 컨트롤러 지도 연결

--- src/components/DetailController.tsx
+++ src/components/DetailController.tsx
@@ -1,13 +1,20 @@
-const DetailController = () => {
+const DetailController = ({ hospitalName }: { hospitalName: string }) => {
   return (
-    <div className="gap-[10px] fixed flex flex-col items-center justify-around right-[20px] top-1/3 w-[80px] h-[150px] p-[10px] bg-[#4095BD] rounded-xl">
+    <div className="gap-[10px] fixed flex flex-col items-center justify-around right-[20px] top-3/4 w-[80px] h-[150px] p-[10px] bg-[#4095BD] rounded-xl">
       <button type="button" className=" text-white text-[15px] font-medium">
         병상정보
       </button>
       <button type="button" className=" text-white text-[15px] font-medium">
         이미지
       </button>
-      <button type="button" className=" text-white text-[15px] font-medium">
+      <button
+        type="button"
+        className=" text-white text-[15px] font-medium"
+        onClick={(e) => {
+          e.preventDefault();
+          window.open(`https://map.naver.com/p/search/${hospitalName}`);
+        }}
+      >
         지도뷰어
       </button>
     </div>
