52350303efbe077a589c1a88500ca214734755e9
george <hyunrim03@naver.com>
2024-05-08 20:30:48 +0900

    feat: 병원 전체 리스트 추가

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import Result from './pages/Result';
 import getPermission from './util/getPermission';
 import SelectionLocation from './pages/SelectionLocation';
 import Detail from './pages/Detail';
+import Hospitals from './pages/Hospitals';
 
 const Router = () => {
   const [locationPermission, setLocationPermission] = useState<boolean | null>(
@@ -23,6 +24,7 @@ const Router = () => {
       <Route path="/select" element={<SelectionLocation />} />
       <Route path="/result" element={<Result />} />
       <Route path="/result/:hospitalCode" element={<Detail />} />
+      <Route path="/hospitals" element={<Hospitals />} />
     </Routes>
   );
 };
