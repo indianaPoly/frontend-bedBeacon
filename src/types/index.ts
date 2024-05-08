fd2831b2b0219a18f4f75205a3856d0e8c00f522
george <hyunrim03@naver.com>
2024-05-08 20:34:59 +0900

    fix: 타입 수정 및 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,32 @@
 export type OS = 'Windows' | 'MacOS' | 'iOS' | 'Android' | 'Linux' | 'Unknown';
 
+export type Room = {
+  roomName: string;
+  availableRooms: number;
+};
+
+type SurgeryUnit = {
+  surgeryName: string;
+  feasible: boolean;
+};
+
+export type MedicalFeasibilityIndex = Array<SurgeryUnit>;
+
 export interface HospitalDataProp {
   name: string;
-  emergencyGeneralWard: number;
-  generalWard: number;
+  emergencyGeneralWard?: number;
+  generalWard?: number;
   distance: number;
   location: string;
   callNumber: string;
   hospitalCode: string;
-  image: string;
+  image?: string;
+}
+
+export interface DetailHospitalDataProp extends HospitalDataProp {
+  hospitalLink: string;
+  hospitalOpen: boolean;
+  hospitalRoomData: Room[];
 }
 
 export interface Position {
