e2be8d8ffa1083abc4e1c2033d218e653eaab870
george <hyunrim03@naver.com>
2024-05-08 20:33:23 +0900

    feat: 사이드 바 제작

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,12 @@
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import menuIcon from '../assets/icons/ic_list_line.svg';
+import SideBar from './SideBar';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [onClickMenu, setOnClickMenu] = useState(false);
+
   return (
     <header className="flex w-full justify-center items-center">
       <div className="flex w-[901px] min-w-[350px] justify-between px-[25px] py-[20px] ">
@@ -17,8 +21,18 @@ const Header = () => {
             Bed Beacon
           </span>
         </button>
-        <img src={menuIcon} alt="햄버거 메뉴" />
+        <button
+          type="button"
+          onClick={() => {
+            setOnClickMenu(!onClickMenu);
+          }}
+        >
+          <img src={menuIcon} alt="햄버거 메뉴" />
+        </button>
       </div>
+      {onClickMenu && (
+        <SideBar onClickMenu={onClickMenu} setOnClickMenu={setOnClickMenu} />
+      )}
     </header>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,32 @@ export default {
         mobile: { min: '0px', max: '900px' },
         deskTop: { min: '901px'}
       },
+      keyframes: {
+        slideDown: {
+          '0%': {
+            transform: 'translateY(-100%)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1',
+          }
+        },
+        slideUp: {
+          '0%': {
+            transform: 'translateY(0)',
+            opacity: '1',
+          },
+          '100%': {
+            transform: 'translateY(-100%)',
+            opacity: '0',
+          }
+        }
+      },
+      animation: {
+        slideDown: 'slideDown 1s ease-in-out',
+        slideUp: 'slideUp 1s ease-in-out'
+      }
     },
   },
   plugins: [],
