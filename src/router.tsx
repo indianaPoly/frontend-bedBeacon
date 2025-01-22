827e43cc2d633564761798184603dd80eabd7b74
indianapoly <hyeonlimgo5@gmail.com>
2025-01-22 17:42:33 +0900

    fix: router error handling

--- src/router.tsx
+++ src/router.tsx
@@ -4,8 +4,6 @@ import Initial from './pages/Initial';
 import Result from './pages/Result';
 import getPermission from './util/getPermission';
 import SelectionLocation from './pages/SelectionLocation';
-import Detail from './pages/Detail';
-import Hospitals from './pages/Hospitals';
 
 const Router = () => {
   const [locationPermission, setLocationPermission] = useState<boolean | null>(
@@ -27,8 +25,6 @@ const Router = () => {
       <Route path="/" element={<Initial isPermission={locationPermission} />} />
       <Route path="/select" element={<SelectionLocation />} />
       <Route path="/result" element={<Result />} />
-      <Route path="/result/:hospitalCode" element={<Detail />} />
-      <Route path="/hospitals" element={<Hospitals />} />
     </Routes>
   );
 };
